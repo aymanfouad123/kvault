17b9be856e82e040fe0be877d7895d6ac8d06d83
aymanfouad123 <aymanfouad033@gmail.com>
2025-06-04 18:50:02 -0400

    feat: adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Manager from "./components/Manager";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       <Navbar />
       <Manager />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="bg-black flex justify-center items-center text-center py-4 mt-8 absolute bottom-0 w-full">
+      <div className="text-zinc-400 text-sm">
+        <p>&copy; {currentYear} KVault. All rights reserved.</p>
+        <p className="text-xs mt-1 text-zinc-500">
+          Your Self Hosted Password Manager
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
