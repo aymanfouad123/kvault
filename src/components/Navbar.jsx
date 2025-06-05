c152c53f6a6bb94a3df41c88bc2eb25434e7887e
aymanfouad123 <aymanfouad033@gmail.com>
2025-06-05 16:03:21 -0400

    fix: nav update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -7,9 +8,15 @@ const Navbar = () => {
         <span className="text-violet-50 font-medium">K</span>
         <span className="text-violet-600 font-bold">Vault</span>
       </div>
-      <div className="navButtons hover:text-gray-300 cursor-pointer transition-colors duration-200 hover:text-[16.5px] w-[55px] flex justify-center">
-        Home
-      </div>
+      <a
+        href="https://github.com/aymanfouad123/kvault"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2 bg-zinc-800 hover:bg-zinc-700 text-white px-3 py-1 rounded-lg transition-colors duration-200 text-base font-light shadow hover:shadow-lg"
+      >
+        <FaGithub />
+        Github
+      </a>
     </div>
   );
 };
