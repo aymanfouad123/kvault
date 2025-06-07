36e2254f9470e1df85b500e3f00ab375a4cfbd18
aymanfouad123 <aymanfouad033@gmail.com>
2025-06-07 19:22:24 -0400

    chore: crud setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { MongoClient } = require("mongodb");
+const bodyparser = require("body-parser");
+const { ObjectId } = require("mongodb");
 
 dotenv.config();
 
@@ -11,14 +13,29 @@ const client = new MongoClient(url);
 const dbName = "kvault";
 const app = express();
 const port = 3000;
+app.use(bodyparser.json());
 
-client.connect();
-
-app.get("/", async (req, res) => {
+app.get("/passwords", async (req, res) => {
   const db = client.db(dbName);
-  const collection = db.collection("documents");
-  const findResult = await collection.find({}).toArray();
-  res.json(findResult);
+  const collection = db.collection("passwords");
+  const passwords = await collection.find({}).toArray();
+  res.json(passwords);
+});
+
+app.post("/passwords", async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection("passwords");
+  const result = await collection.insertOne(req.body);
+  res.json(result);
+});
+
+app.delete("/passwords/:id", async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection("passwords");
+  const result = await collection.deleteOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.json(result);
 });
 
 app.listen(port, () => {
